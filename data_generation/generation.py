0a767ad1f49daf106e69c9e177eb04c1541a9a77
Ben Brooks <bjbrooks@usc.edu>
2019-01-29 01:40:05 -0800

    removed print statements

--- data_generation/generation.py
+++ data_generation/generation.py
@@ -10,7 +10,6 @@ lang.close()
 shell = open('data_generation/generation.sh', 'w')
 shell.write('python TextRecognitionDataGenerator/TextRecognitionDataGenerator/run.py --output_dir data_generation/positive_samples -l sp -c 750 -w 1 -bl 3 -rbl -k 45 -rk\n')
 shell.write('python TextRecognitionDataGenerator/TextRecognitionDataGenerator/run.py --output_dir data_generation/false_samples -l en -c 6750 -w 1 -bl 3 -rbl -k 45 -rk\n')
-print("File created")
 shell.close()
     
         
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 python data_generation/generation.py $1
-cat TextRecognitionDataGenerator/TextRecognitionDataGenerator/dicts/sp.txt
 ./data_generation/generation.sh
