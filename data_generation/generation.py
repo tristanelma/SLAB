104f7dea50c4667b32239456c68c4873f6d20efe
Ben Brooks <bjbrooks@usc.edu>
2019-01-29 01:39:20 -0800

    updating where we save photos

--- data_generation/generation.py
+++ data_generation/generation.py
@@ -8,8 +8,8 @@ lang.write(sample_word + '\n')
 lang.close()
 
 shell = open('data_generation/generation.sh', 'w')
-shell.write('python TextRecognitionDataGenerator/TextRecognitionDataGenerator/run.py --output_dir positive_samples -l sp -c 750 -w 1 -bl 3 -rbl -k 45 -rk\n')
-shell.write('python TextRecognitionDataGenerator/TextRecognitionDataGenerator/run.py --output_dir false_samples -l en -c 6750 -w 1 -bl 3 -rbl -k 45 -rk\n')
+shell.write('python TextRecognitionDataGenerator/TextRecognitionDataGenerator/run.py --output_dir data_generation/positive_samples -l sp -c 750 -w 1 -bl 3 -rbl -k 45 -rk\n')
+shell.write('python TextRecognitionDataGenerator/TextRecognitionDataGenerator/run.py --output_dir data_generation/false_samples -l en -c 6750 -w 1 -bl 3 -rbl -k 45 -rk\n')
 print("File created")
 shell.close()
     
