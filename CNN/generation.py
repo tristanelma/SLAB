9803366249a853fa5bc58a0b6da8f3c51cc63b3c
Ben Brooks <bjbrooks@usc.edu>
2019-01-31 21:52:03 -0800

    more readable

--- CNN/generation.py
+++ CNN/generation.py
@@ -21,37 +21,28 @@ unaltered_split = 0.5
 blurred_split = 0.25
 drawn_split = 0.25
 
-subprocess.call(['rm', '-rf', 'data_generation/training'])
-subprocess.call(['rm', '-rf', 'data_generation/testing'])
+subprocess.call(['rm', '-rf', 'data_generation'])
+subprocess.call(['mkdir', 'data_generation'])
+subprocess.call(['rm', '-rf', 'data_generation/positive_samples'])
+subprocess.call(['rm', '-rf', 'data_generation/false_samples'])
 
 # Path to textrecognitiondatagenerator
 DATAGEN_LOC = 'TextRecognitionDataGenerator/TextRecognitionDataGenerator/run.py'
 
 #training
 # unaltered files get name format 0
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/training/positive_samples' , '-na' , '0' ,'-l', 'sp', '-c', str(int(total_size*positive_split*training_split*unaltered_split)), '-w', '1'])
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/training/false_samples' , '-na' , '0' ,'-l', 'en', '-c', str(int(total_size*negative_split*training_split*unaltered_split)), '-w', '1'])
+subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/positive_samples' , '-na' , '0' ,'-l', 'sp', '-c', str(int(total_size*positive_split*unaltered_split)), '-w', '1'])
+subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/false_samples' , '-na' , '0' ,'-l', 'en', '-c', str(int(total_size*negative_split*unaltered_split)), '-w', '1'])
 # blurred files get name format 1
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/training/positive_samples', '-na', '1', '-l', 'sp', '-c', str(int(total_size*positive_split*training_split*blurred_split)), '-w', '1', '-bl', '2', '-rbl', '-k', '45', '-rk'])
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/training/false_samples', '-na', '1', '-l', 'en', '-c', str(int(total_size*negative_split*training_split*blurred_split)), '-w', '1', '-bl', '2', '-rbl', '-k', '45', '-rk'])
+subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/positive_samples', '-na', '1', '-l', 'sp', '-c', str(int(total_size*positive_split*blurred_split)), '-w', '1', '-bl', '2', '-rbl', '-k', '45', '-rk'])
+subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/false_samples', '-na', '1', '-l', 'en', '-c', str(int(total_size*negative_split*blurred_split)), '-w', '1', '-bl', '2', '-rbl', '-k', '45', '-rk'])
 # manually altered files get name format 2
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/training/alter_pos', '-na', '2', '-l', 'sp', '-c', str(int(total_size*positive_split*training_split*drawn_split)), '-w', '1', '-k', '45', '-rk'])
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/training/alter_false', '-na', '2', '-l', 'en', '-c', str(int(total_size*negative_split*training_split*drawn_split)), '-w', '1', '-k', '45', '-rk'])
-
-#testing
-# unaltered files get name format 0
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/testing/positive_samples' , '-na' , '0' ,'-l', 'sp', '-c',str(int(total_size*positive_split*test_split*unaltered_split)), '-w', '1'])
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/testing/false_samples' , '-na' , '0' ,'-l', 'en', '-c', str(int(total_size*negative_split*test_split*unaltered_split)), '-w', '1'])
-# blurred files get name format 1
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/testing/positive_samples', '-na', '1', '-l', 'sp', '-c', str(int(total_size*positive_split*test_split*blurred_split)), '-w', '1', '-bl', '2', '-rbl', '-k', '45', '-rk'])
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/testing/false_samples', '-na', '1', '-l', 'en', '-c', str(int(total_size*negative_split*test_split*blurred_split)), '-w', '1', '-bl', '2', '-rbl', '-k', '45', '-rk'])
-# manually altered files get name format 2
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/testing/alter_pos', '-na', '2', '-l', 'sp', '-c', str(int(total_size*positive_split*test_split*drawn_split)), '-w', '1', '-k', '45', '-rk'])
-subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/testing/alter_false', '-na', '2', '-l', 'en', '-c', str(int(total_size*negative_split*test_split*drawn_split)), '-w', '1', '-k', '45', '-rk'])
+subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/alter_pos', '-na', '2', '-l', 'sp', '-c', str(int(total_size*positive_split*drawn_split)), '-w', '1', '-k', '45', '-rk'])
+subprocess.call(['python', DATAGEN_LOC, '--output_dir', 'data_generation/alter_false', '-na', '2', '-l', 'en', '-c', str(int(total_size*negative_split*drawn_split)), '-w', '1', '-k', '45', '-rk'])
 
 #altering positive samples
-for i in range(int(total_size*positive_split*training_split*drawn_split)):
-    img = Image.open('data_generation/training/alter_pos/%d.jpg' % i)
+for i in range(int(total_size*positive_split*drawn_split)):
+    img = Image.open('data_generation/alter_pos/%d.jpg' % i)
     w = random.randint(1, img.size[1]/2)
     x1 = random.randint(0, img.size[0])
     y1 = random.randint(0, img.size[1])
@@ -59,22 +50,11 @@ for i in range(int(total_size*positive_split*training_split*drawn_split)):
     y2 = random.randint(0, img.size[1])
     img_draw = ImageDraw.Draw(img)
     img_draw.line([(x1, y1), (x2, y2)], width=w, fill='brown')
-    img.save('data_generation/training/positive_samples/%d.jpg' % i)
-
-for i in range(int(total_size*positive_split*test_split*drawn_split)):
-    img = Image.open('data_generation/testing/alter_pos/%d.jpg' % i)
-    w = random.randint(1, img.size[1]/2)
-    x1 = random.randint(0, img.size[0])
-    y1 = random.randint(0, img.size[1])
-    x2 = random.randint(0, img.size[0])
-    y2 = random.randint(0, img.size[1])
-    img_draw = ImageDraw.Draw(img)
-    img_draw.line([(x1, y1), (x2, y2)], width=w, fill='brown')
-    img.save('data_generation/testing/positive_samples/%d.jpg' % i)
+    img.save('data_generation/positive_samples/%d.jpg' % i)
 
 #altering false samples
-for x in range(int(total_size*negative_split*training_split*drawn_split)):
-    img = Image.open('data_generation/training/alter_false/%d.jpg' % x)
+for x in range(int(total_size*negative_split*drawn_split)):
+    img = Image.open('data_generation/alter_false/%d.jpg' % x)
     w = random.randint(1, img.size[1]/2)
     x1 = random.randint(0, img.size[0])
     y1 = random.randint(0, img.size[1])
@@ -82,23 +62,10 @@ for x in range(int(total_size*negative_split*training_split*drawn_split)):
     y2 = random.randint(0, img.size[1])
     img_draw = ImageDraw.Draw(img)
     img_draw.line([(x1, y1), (x2, y2)], width=w, fill='brown')
-    img.save('data_generation/training/false_samples/%d.jpg' % x)
+    img.save('data_generation/false_samples/%d.jpg' % x)
 
-for x in range(int(total_size*negative_split*test_split*drawn_split)):
-    img = Image.open('data_generation/testing/alter_false/%d.jpg' % x)
-    w = random.randint(1, img.size[1]/2)
-    x1 = random.randint(0, img.size[0])
-    y1 = random.randint(0, img.size[1])
-    x2 = random.randint(0, img.size[0])
-    y2 = random.randint(0, img.size[1])
-    img_draw = ImageDraw.Draw(img)
-    img_draw.line([(x1, y1), (x2, y2)], width=w, fill='brown')
-    img.save('data_generation/testing/false_samples/%d.jpg' % x)
-
-subprocess.call(['rm', '-rf', 'data_generation/training/alter_false'])
-subprocess.call(['rm', '-rf', 'data_generation/training/alter_pos'])
-subprocess.call(['rm', '-rf', 'data_generation/testing/alter_false'])
-subprocess.call(['rm', '-rf', 'data_generation/testing/alter_pos'])
+subprocess.call(['rm', '-rf', 'data_generation/alter_false'])
+subprocess.call(['rm', '-rf', 'data_generation/alter_pos'])
     
 
 
