11a0e632ae5284cb4b8f7fd199da2363c89412b2
Chengyi (Jeff) Chen <chen364@usc.edu>
2019-04-03 19:45:32 -0700

    Jeff: Added the edited process_panos.py that Tirstan edited

--- process_panos.py
+++ process_panos.py
@@ -19,7 +19,7 @@ import pandas as pd
 
 # Helper function to create a new folder
 def mkdir(path):
-    try: 
+    try:
         os.makedirs(path)
     except OSError:
         if not os.path.isdir(path):
@@ -49,7 +49,7 @@ def sliding_window(image, step_size=300, window_size=(600, 600)):
 
 # Main function
 if __name__ == '__main__':
-    
+
     # Read in arguments from command line
     parser = argparse.ArgumentParser(
         description='Generates 600 x 600 sized images derived from original panoramic image.'
@@ -57,12 +57,12 @@ if __name__ == '__main__':
     parser.add_argument('data_dir', metavar='DATA-DIRECTORY', action='store', help='Path to the data directory with panoramic images to be used for generating smaller images.')
     parser.add_argument('--save_dir', metavar='SAVE-DIRECTORY', action='store', help='Set directory to save all the smaller images derived from the processed panoramic image.')
     settings = parser.parse_args()
-    
+
     # Store all the image names into list
     image_names = [f for f in listdir(settings.data_dir) if isfile(join(settings.data_dir, f))]
-    
+
     # print(image_names)
-    
+
     # Create directory to store the processed panoramic images
     if settings.save_dir != None:
         save_dir = settings.save_dir
@@ -70,16 +70,17 @@ if __name__ == '__main__':
         save_dir = './processed_panos/'
     mkdir(save_dir)
 
-    # Loop through all the image names to create the 
+    # Loop through all the image names to create the
     # smaller images from that directory of panoramic images
     for image_name in image_names:
-        
+
         img = cv2.imread(join(settings.data_dir, image_name))
+
+        pano = Image.open(join(settings.data_dir, image_name))
 
         # Loop through each sliding window created
         for idx, window in tqdm(enumerate(sliding_window(img, 300, (600, 600)))):
-            Image.open(join(settings.data_dir, image_name)).crop(window).save(join(settings.save_dir, '{}_{}.jpg'.format(image_name[:-image_name.rfind('.')], idx)))
-                                                                              
-                                                                              
+            pano.crop(window).save(join(save_dir, '{}_{}.jpg'.format(image_name[:-4], idx)))
+
+
     print('All panoramic images in {} have been processed.'.format(settings.data_dir))
-    
